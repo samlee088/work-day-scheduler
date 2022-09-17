e25929979d1ffc55ad58055998ae9a1b4cf1576d
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-09-17 02:43:34 -0700

    layout for HTML for times laid out

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var today = moment();
+$('#currentDay').text(today.format("dddd"));
+
+
+
+/* make either a HTML text connect with jquery, or use bootstrap to make a variable table */
+
+
+/* breakout is time, 8-5 w/ 1-hr lunch, middle action item for time, and save button */
+
+/* after the save, it will save to local storage so that it reloads after refresh on the page */
+/* save with either array, or with individual save states? */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
